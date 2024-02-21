91df5c8b71a73227818fc7ad58ade92091fc80d3
saleem7861 <ms4546811@gmail.com>
2024-02-21 14:12:43 +0500

    qq

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col text-blue-900  font-bold  items-center justify-between p-24">
+     <div> Web development</div>
+    </main>
+  );
+}
